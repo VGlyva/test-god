2e8a26fd499beeca082617e35ee10d98a8237322
VGlyva <135941893+VGlyva@users.noreply.github.com>
2024-07-27 14:02:42 +0700

    Мой первый коммит

---
+++ src/main/java/com/test/human/god/GodApplication.java
@@ -0,0 +1,13 @@
+package com.test.human.god;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodApplication.class, args);
+	}
+
+}
